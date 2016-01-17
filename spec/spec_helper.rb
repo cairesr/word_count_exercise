29fb7e266ebb803168616eb8fbe446e6e17478de
Rodrigo Gai <rcrosse@walmart.com>
2016-01-17 12:33:18 -0200

    Add lib path into spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+Dir['lib/*.rb'].each { |f| require File.expand_path(f) }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
