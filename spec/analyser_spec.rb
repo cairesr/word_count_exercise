0d69455eb8121c0a29fdace113a4f7c34e30c6d6
Rodrigo Gai <rcrosse@walmart.com>
2016-01-17 17:39:00 -0200

    Refactor the Analyzer context in analyzer_spec

--- spec/analyser_spec.rb
+++ spec/analyser_spec.rb
@@ -1,25 +1,27 @@
 require 'spec_helper'
 
-RSpec.describe Analyzer, "#validate_url" do
-  context 'with a nil url' do
-    subject { Analyzer.new(nil) }
+RSpec.describe Analyzer do
+  context '#validate_url' do
+    context 'with a nil url' do
+      subject { Analyzer.new(nil) }
 
-    it 'raises an ArgumentError' do
-      expect { subject.validate_url }.to raise_error(ArgumentError)
+      it 'raises an ArgumentError' do
+        expect { subject.validate_url }.to raise_error(ArgumentError)
+      end
     end
-  end
 
-  context 'with an invalid url' do
-    subject { Analyzer.new('http:/|www.luv.com') }
+    context 'with an invalid url' do
+      subject { Analyzer.new('http:/|www.luv.com') }
 
-    it 'raises URI::InvalidURIError' do
-      expect { subject.validate_url }.to raise_error(URI::InvalidURIError)
+      it 'raises URI::InvalidURIError' do
+        expect { subject.validate_url }.to raise_error(URI::InvalidURIError)
+      end
     end
-  end
 
-  context 'with the default url attribute' do
-    it 'uses the default url value' do
-      expect(subject.validate_url).to eq(subject.remote_file_url)
+    context 'with the default url attribute' do
+      it 'uses the default url value' do
+        expect(subject.validate_url).to eq(subject.remote_file_url)
+      end
     end
   end
 end
